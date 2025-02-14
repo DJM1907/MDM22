d1b9f7b891fa1950f3e4a4159f7d55ee26e34810
Josie Scott-Fleming <josie.scottfleming@gmail.com>
2025-02-14 10:39:31 +0000

    Now generates a 3d model of the hull as opposed to plotting it as a point cloud.

--- mesh_reader.py
+++ mesh_reader.py
@@ -26,11 +26,22 @@ X = np.concatenate((X_bow,X_stn))
 Y = np.concatenate((Y_bow,Y_stn))
 Z = np.concatenate((Z_bow,Z_stn))
 
-mesh = np.array([X,Y,Z]).transpose()
+mesh1 = np.array([X,Y,Z]).transpose()
+mesh2 = np.array([X,-Y,Z]).transpose()
+mesh = np.concatenate((mesh1,mesh2))
 
-pl = pv.Plotter()
-pl.add_mesh(mesh, color='black')
-pl.show()
+# Plotting as a surface using delaunay triangulation
+# cloud = pv.PolyData(mesh)
+# surf = cloud.delaunay_2d()
+# surf.plot()
+
+# Plotting as a surface using pyvista stuctured surfaces
+#X,Y,Z = np.meshgrid(X,Y,Z)
+X = np.concatenate((X,X))
+Y = np.concatenate((Y,-Y))
+Z = np.concatenate((Z,Z))
+grid = pv.StructuredGrid(X,Y,Z)
+grid.plot()
 
 # Improve model by first mirroring so it's a full hull
 # then work out how to make it into a full mesh instead of a point cloud
